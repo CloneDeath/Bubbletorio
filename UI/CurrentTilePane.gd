6bb1a0a72fc17a9538d5b37e1536741b4f572479
Nicholas Rodine <halfofastaple@gmail.com>
2019-07-27 14:10:00 -0400

    Fixed crash caused by infinite recursion.

--- UI/CurrentTilePane.gd
+++ UI/CurrentTilePane.gd
@@ -1,4 +1,5 @@
 extends VBoxContainer
 
 func set_current_tile(tile):
-	self.propagate_call("set_current_tile", [tile]);
+	for child in get_children():
+		child.propagate_call("set_current_tile", [tile]);
