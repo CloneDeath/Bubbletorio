c6d8d3eb39dab5f398b27977254ddc8168eb50eb
Nicholas Rodine <halfofastaple@gmail.com>
2019-07-27 14:15:43 -0400

    Added bubble level to pipes and pumps.

--- UI/CurrentTileInfo/Tiles/PumpInfo/PumpInfo.gd
+++ UI/CurrentTileInfo/Tiles/PumpInfo/PumpInfo.gd
@@ -2,3 +2,10 @@ extends "res://UI/CurrentTileInfo/CurrentTileInfo.gd"
 
 func handles_tile(tile)->bool:
 	return tile.is_in_group("tile_pump");
+
+
+func _process(_delta):
+	if (current_tile == null): return;
+	$Properties/BubblesValue.text = \
+		str(current_tile.bubble_count, "/", current_tile.max_bubbles, 
+		" Bubbles");
